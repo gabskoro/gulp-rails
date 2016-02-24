78f5c0d00bed5e054fb29b8bc840e715554021b1
Gabrijel Škoro <gabrijel.skoro@gmail.com>
2016-02-24 22:53:06 +0100

    Add watcher for js

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,7 +7,7 @@ concat     = require('gulp-concat');
 uglify     = require('gulp-uglify');
 rev        = require('gulp-rev');
 
-gulp.task('default', ['compile-scss']);
+gulp.task('default', ['compile-scss', 'compile-js']);
 
 gulp.task('compile-scss', function() {
   gulp.src('app/assets/stylesheets/**/*.scss')
@@ -19,7 +19,14 @@ gulp.task('compile-scss', function() {
       .pipe(livereload());
 });
 
+gulp.task('compile-js', function() {
+  gulp.src('app/assets/javascripts/**/*.js')
+      .pipe(gulp.dest('public/assets'))
+      .pipe(livereload());
+});
+
 gulp.task('watch', function() {
   livereload.listen({reloadPage: 'app/views/layouts/application.html'});
   gulp.watch('app/assets/stylesheets/**/*.scss', ['compile-scss']);
+  gulp.watch('app/assets/javascripts/**/*.js', ['compile-js']);
 });
