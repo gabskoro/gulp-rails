8118201cc58bc30b4f42ffe1cea34cac9abe2175
Gabrijel Škoro <gabrijel.skoro@gmail.com>
2016-02-24 22:26:24 +0100

    add some gulp tasks

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+var gulp, sass, livereload, concat, uglify, rev;
+
+gulp       = require('gulp');
+sass       = require('gulp-sass');
+livereload = require('gulp-livereload');
+concat     = require('gulp-concat');
+uglify     = require('gulp-uglify');
+rev        = require('gulp-rev');
+
+gulp.task('default', ['compile-scss']);
+
+gulp.task('compile-scss', function() {
+  gulp.src('app/assets/stylesheets/**/*.scss')
+      .pipe(sass({ indentedSyntax: false, errLogToConsole: true }))
+      // .pipe(rev())
+      .pipe(gulp.dest('public/assets'))
+      // .pipe(rev.manifest())
+      // .pipe(gulp.dest('public/assets'))
+      .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  livereload.listen({reloadPage: 'app/views/layouts/application.html'});
+  gulp.watch('app/assets/stylesheets/**/*.scss', ['compile-scss']);
+});
